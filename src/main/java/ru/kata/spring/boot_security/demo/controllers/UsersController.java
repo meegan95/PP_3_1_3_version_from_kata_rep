85b5aa30a1e43648a0d03b04237a244b912b35be
meegan95 <122483262+meegan95@users.noreply.github.com>
2023-03-19 15:58:19 +0300

    crud

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UsersController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UsersController.java
@@ -1,52 +1,76 @@
-//package ru.kata.spring.boot_security.demo.controllers;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.servlet.ModelAndView;
-//import ru.kata.spring.boot_security.demo.models.Role;
-//import ru.kata.spring.boot_security.demo.models.User;
-//import ru.kata.spring.boot_security.demo.repositories.RolesRepository;
-//import ru.kata.spring.boot_security.demo.services.UsersDetailsService;
-//
-//import java.util.List;
-//@Controller
-//public class UsersController {
-//
-//    private UsersDetailsService usersDetailsService;
-//    private RolesRepository rolesRepository;
-//
-//    @Autowired
-//    public UsersController(UsersDetailsService usersDetailsService, RolesRepository rolesRepository) {
-//        this.usersDetailsService = usersDetailsService;
-//        this.rolesRepository = rolesRepository;
-//    }
-//
-//    @GetMapping("/users/new")
-//    public ModelAndView newUser() {
-//        User user = new User();
-//        ModelAndView mav = new ModelAndView("user_form");
-//        mav.addObject("user", user);
-//
-//        List<Role> roles = (List<Role>) rolesRepository.findAll();
-//
-//        mav.addObject("allRoles", roles);
-//
-//        return mav;
-//    }
-//
-////    @GetMapping("/users/edit/{id}")
-////    public ModelAndView editUser(@PathVariable(name = "id") Integer id) {
-//////        User user = usersDetailsService.loadUserByUsername()
-//////                service.get(id);
-////        ModelAndView mav = new ModelAndView("user_form");
-////        mav.addObject("user", user);
-////
-////        List<Role> roles = (List<Role>) rolesRepository.findAll();
-////
-////        mav.addObject("allRoles", roles);
-////
-////        return mav;
-////    }
-//}
+package ru.kata.spring.boot_security.demo.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/users")
+public class UsersController {
+
+
+    private final UserService userService;
+
+    @Autowired
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping()
+    public String index(Model model){
+        model.addAttribute("users", userService.findAll());
+        return "users/index";
+
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userService.findOne(id));
+        return "users/show";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user) {
+        return "users/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "users/new";
+        }
+        userService.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("user", userService.findOne(id));
+        return "users/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult
+            , @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "users/edit";
+        }
+        userService.update(id, user);
+        return "redirect:/users";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        userService.delete(id);
+        return "redirect:/users";
+    }
+
+}
