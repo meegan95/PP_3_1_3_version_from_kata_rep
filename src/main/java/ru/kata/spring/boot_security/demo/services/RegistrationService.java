2aed65a7afa5ab282ecaa6f406c81606e84febaf
meegan95 <122483262+meegan95@users.noreply.github.com>
2023-03-27 16:40:20 +0300

    encode passwords

--- src/main/java/ru/kata/spring/boot_security/demo/services/RegistrationService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RegistrationService.java
@@ -1,6 +1,7 @@
 package ru.kata.spring.boot_security.demo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.models.Role;
@@ -14,14 +15,14 @@ public class RegistrationService {
     private final UsersRepository usersRepository;
 
     @Autowired
-    public RegistrationService(UsersRepository usersRepository
-    ) {
+    public RegistrationService(UsersRepository usersRepository) {
         this.usersRepository = usersRepository;
     }
 
     @Transactional
     public void register(User user) {
         user.addRole(new Role("ROLE_USER"));
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
         usersRepository.save(user);
     }
 
