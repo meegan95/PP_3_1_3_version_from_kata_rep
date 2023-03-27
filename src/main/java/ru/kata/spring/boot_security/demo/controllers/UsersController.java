4e67bf51aa0264037354ad23f71a312079642f15
meegan95 <122483262+meegan95@users.noreply.github.com>
2023-03-27 19:59:54 +0300

    interface user service

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UsersController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UsersController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.services.UserService;
 import ru.kata.spring.boot_security.demo.services.UserServiceImpl;
 
 import java.security.Principal;
@@ -13,16 +14,16 @@ import java.security.Principal;
 @RequestMapping("/user")
 public class UsersController {
 
-    private final UserServiceImpl userServiceImpl;
+    private final UserService userService;
 
     @Autowired
     public UsersController(UserServiceImpl userServiceImpl) {
-        this.userServiceImpl = userServiceImpl;
+        this.userService = userServiceImpl;
     }
 
     @GetMapping
     public String show(Principal principal, Model model) {
-        model.addAttribute("user", userServiceImpl.getPersonByUsername(principal.getName()));
+        model.addAttribute("user", userService.getPersonByUsername(principal.getName()));
         return "/user/show";
     }
 
